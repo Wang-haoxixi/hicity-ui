152f95c759689590dde695d1b398c6de8ef2ffb2
雷春路 <cc32131914@163.com>
2021-02-03 15:06:44 +0800

    游记init

---
+++ src/views/components/HcTopicSelect/index.vue
@@ -0,0 +1,203 @@
+<template>
+  <div>
+    <el-input @focus="toSelect" :value="topicShow" readonly></el-input>
+    <el-dialog
+      title="话题分类选择"
+      :visible.sync="classifyChooseDialogVisible"
+      width="90%">
+      <div>
+        <div class="classify-box">
+          <div v-for="classify in classifyList" :key="classify.id" class="classify-item" @click="selectClassify(classify.id)">
+            <div class="classify-item-name">{{classify.classifyName}}</div>
+            <el-image class="classify-item-cover" :src="classify.imageUrl"></el-image>
+          </div>
+        </div>
+        <div class="pagination-box">
+          <el-pagination
+            style="display: inline-block"
+            @size-change="sizeChange"
+            @current-change="currentChange"
+            :current-page="page.currentPage"
+            :page-sizes="[10, 20, 30,, 40, 50, 100]"
+            background
+            :page-size="page.pageSize"
+            layout="total, sizes, prev, pager, next, jumper"
+            :total="page.total">
+          </el-pagination>
+        </div>
+      </div>
+      <div slot="footer">
+        <el-button @click="classifyChooseDialogVisible = false">取 消</el-button>
+      </div>
+    </el-dialog>
+
+    <el-dialog
+      title="话题选择"
+      :visible.sync="topicChooseDialogVisible"
+      width="70%"
+    >
+      <div class="classify-list">
+        <div v-for="topic in relativeTopicList"
+          :key="topic.id"
+          class="classify-list-item"
+          @click="handleSelect(topic)">
+          {{ topic.topicsName }}
+        </div>
+      </div>
+      <div slot="footer">
+        <el-button @click="topicChooseDialogVisible = false">取 消</el-button>
+      </div>
+    </el-dialog>
+  </div>
+  
+</template>
+
+<script>
+import { getClassifyList, getTopicList } from '@/api/cms/travel'
+export default {
+  props: {
+    topicName: {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      topicShow: '',
+      classifyList: [],
+      page: {
+        currentPage: 1,
+        pageSize: 20,
+        total: 0,
+      },
+      classifyChooseDialogVisible: false,
+      relativeTopicList: [],
+      topicChooseDialogVisible: false,
+    }
+  },
+  created () {
+    this.topicShow = this.topicName
+  },
+  watch: {
+    topicName (value) {
+      this.topicShow = value
+    }
+  },
+  methods: {
+    getList (cb) {
+      this.tableLoading = true
+      getClassifyList({
+        current: this.page.currentPage,
+        size: this.page.pageSize,
+      }).then(({data}) => {
+        if (data.code === 0) {
+          this.classifyList = data.data.data.records
+          this.page.total = data.data.data.total
+        }
+      }).finally(() => {
+        this.tableLoading = false
+        cb && cb()
+      })
+    },
+    currentChange (current) {
+      this.page.currentPage = current
+      this.getList()
+    },
+    sizeChange (size) {
+      this.page.pageSize = size
+      this.page.currentPage = 1
+      this.getList()
+    },
+    toSelect () {
+      this.getList(() => {
+        this.classifyChooseDialogVisible = true
+      })
+    },
+    selectClassify (classifyId) {
+      getTopicList({
+        queryType: 1,
+        classifyId
+      }).then(({data}) => {
+        this.relativeTopicList = data.data.data.records
+        this.topicChooseDialogVisible = true;
+      })
+    },
+    handleSelect (topic) {
+      this.classifyChooseDialogVisible = false
+      this.topicChooseDialogVisible = false
+      this.topicShow = topic.topicsName
+      this.$emit('input', [topic.id])
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.classify-box {
+  margin-top: 24px;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 250px);
+  grid-template-rows: repeat(auto-fill, auto);
+  grid-gap: 24px;
+  .classify-item {
+    border-radius: 2px;
+    border: 1px solid #E9E9E9;
+    padding: 16px;
+    .classify-item-name {
+      height: 22px;
+      line-height: 22px;
+      overflow: hidden;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+      text-align: center;
+    }
+    .classify-item-cover {
+      margin-top: 16px;
+      height: 138px;
+      width: 218px;
+    }
+    .classify-item-topic-view {
+      margin-top: 12px;
+      height: 34px;
+      line-height: 34px;
+      border: 1px solid #D9D9D9;
+      border-radius: 5px;
+      color: #666666;
+      font-size: 14px;
+      text-align: center;
+      cursor: pointer;
+    }
+    .classify-item-option {
+      margin-top: 16px;
+      height: 20px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      .mune-item {
+        font-size: 14px;
+      }
+    }
+  }
+}
+
+.pagination-box {
+  padding: 10px 20px;
+  margin: 15px 0 10px;
+  text-align: right;
+}
+
+.classify-list {
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+  .classify-list-item {
+    margin-bottom: 10px;
+    height: 30px;
+    line-height: 30px;
+    padding: 0 15px;
+    border: 1px solid #e9e9e9;
+    border-radius: 4px;
+    margin-right: 20px;
+  }
+}
+</style>
---
+++ src/views/contentManage/travel/index.vue
@@ -0,0 +1,332 @@
+<template>
+  <basic-container>
+    <hc-table-form
+      :title="title"
+      :formVisible="publish"
+      @go-back="publish = false">
+      <avue-crud
+        ref="crud"
+        :option="tableOption"
+        :page="page"
+        :table-loading="tableLoading"
+        :data="tableData"
+        @on-load="getList"
+        @refresh-change="handleRefreshChange"
+      >
+        <template slot="menuLeft">
+          <el-button
+            class="filter-item"
+            type="primary"
+            size="mini"
+            icon="el-icon-plus"
+            @click="toCreate"
+            >添加
+          </el-button>
+        </template>
+        <template slot="cityList" slot-scope="scope">
+          <el-button type="text" size="mini"
+            >查看</el-button
+          >
+        </template>
+        <template slot="menu" slot-scope="scope">
+          <template v-if="isAdmin || (!isAdmin && scope.row.source == 2)">
+            <el-button type="text" size="mini" @click="toUpdate(scope.row)"
+              >编辑</el-button
+            >
+            <el-button type="text" size="mini" @click="toDelete(scope.row)"
+              >删除</el-button
+            >
+          </template>
+        </template>
+      </avue-crud>
+      <template slot="form">
+        <el-form
+          ref="form"
+          class="dialog-main-tree"
+          :model="formData"
+          label-width="180px">
+          <el-form-item label="游记名称：">
+            <el-input v-model="formData.travelName"></el-input>
+          </el-form-item>
+          <el-form-item label="关联话题：">
+            <hc-topic-select v-model="formData.topicsBankIdSet" topic-name="122222"></hc-topic-select>
+          </el-form-item>
+          <el-form-item label="游记图片：">
+            <hc-image-upload v-model="formData.images" :limit="50"></hc-image-upload>
+          </el-form-item>
+          <el-form-item v-if="isAdmin" label="发布城市：">
+            <hc-city-select v-model="formData.cityList"></hc-city-select>
+          </el-form-item>
+          <el-form-item label="内容：">
+            <el-input type="textarea" v-model="formData.content"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button @click="preview">预览</el-button>
+            <el-button @click="handleDraft">保存草稿</el-button>
+            <el-button @click="handleCreate">直接发布</el-button>
+          </el-form-item>
+        </el-form>
+      </template>
+    </hc-table-form>
+    <el-dialog
+      title="展示城市"
+      :visible.sync="cityViewDialogVisible"
+      width="70%"
+    >
+      <hc-city-box
+        view-only
+        :init-city-list="initCityList"
+        :all-city-list="allCityList"
+      ></hc-city-box>
+      <div slot="footer">
+        <el-button @click="cityViewDialogVisible = false">取 消</el-button>
+      </div>
+    </el-dialog>
+  </basic-container>
+</template>
+
+<script>
+import { tableOption } from "./const";
+import { mapGetters } from "vuex";
+import {
+  newsOpenList,
+} from "@/api/cms/news";
+import { getTravelList, getClassifyList, getTopicList, saveTravel, getTravelDetail, deleteTravel } from "@/api/cms/travel"
+import HcCityBox from "@/views/components/HcCityBox/index";
+import HcCitySelect from "@/views/components/HcCitySelect/index";
+import HcImageUpload from "@/views/components/HcImageUpload/index";
+import HcTopicSelect from "@/views/components/HcTopicSelect/index";
+import HcTableForm from "@/views/components/HcTableForm/index";
+export default {
+  name: "SysUser",
+  components: { HcCityBox, HcCitySelect, HcImageUpload, HcTopicSelect, HcTableForm },
+  data() {
+    return {
+      page: {
+        total: 0, // 总页数
+        currentPage: 1, // 当前页数
+        pageSize: 20, // 每页显示多少条,
+        isAsc: false, // 是否倒序
+      },
+      tableLoading: false,
+      tableData: [],
+      formData: {},
+      publish: false,
+      publishType: "",
+      allCityList: [],
+      initCityList: [],
+      cityViewDialogVisible: false,
+      topicProps: {
+        lazy: true,
+        lazyLoad (node, resolve) {
+          const { level } = node;
+          if (level == 0) {
+            getClassifyList({
+              current: 1,
+              size: 1000000
+            }).then(({data}) => {
+              let classifyList = data.data.data.records
+              let nodes = []
+              for (let i = 0; i < classifyList.length; i++) {
+                nodes.push({
+                  value: classifyList[i].id,
+                  label: classifyList[i].classifyName,
+                  leaf: false
+                })
+              }
+              resolve(nodes)
+            })
+          } else {
+            getTopicList({
+              queryType: 1,
+              classifyId: node.value
+            }).then(({data}) => {
+              let topicList = data.data.data.records
+              let nodes = []
+              for (let i = 0; i < topicList.length; i++) {
+                nodes.push({
+                  value: topicList[i].id,
+                  label: topicList[i].topicsName,
+                  leaf: true
+                })
+              }
+              resolve(nodes)
+            })
+          }
+        }
+      }
+    };
+  },
+  computed: {
+    ...mapGetters(["permissions", "userInfo", "dicList"]),
+    tableOption() {
+      return tableOption(this.isAdmin);
+    },
+    isAdmin() {
+      return this.userInfo.userType == 3 || this.userInfo.userType == 4;
+    },
+    title () {
+      if (!this.publish) {
+        return '官方话题'
+      } else {
+        if (this.publishType == 'add') {
+          return '官方话题-新增'
+        } else {
+          return '官方话题-编辑'
+        }
+      }
+    }
+  },
+  created() {
+  },
+  methods: {
+    getList(page = this.page, params) {
+      this.tableLoading = true;
+      let form = {
+        current: page.currentPage,
+        size: page.pageSize,
+        queryType: 0
+      };
+      getTravelList(form)
+        .then(({ data }) => {
+          this.tableData = data.data.data.records;
+          this.page.total = data.data.data.total;
+        })
+        .finally(() => {
+          this.tableLoading = false;
+        });
+    },
+    toCreate() {
+      this.formData = {
+        cityList: [],
+        images: [],
+      };
+      if (!this.isAdmin) {
+        this.formData.cityList = [this.userInfo.manageCityId];
+      }
+      this.publish = true;
+      this.publishType = "add";
+    },
+    handleCreate() {
+      let formData = this.formData;
+      saveTravel({ ...formData, state: 1 }).then(({ data }) => {
+        this.publish = false;
+        this.$notify({
+          title: "成功",
+          message: "发布成功",
+          type: "success",
+          duration: 2000,
+        });
+        this.getList();
+      });
+    },
+    toUpdate({ id }) {
+      getTravelDetail(id).then(({ data }) => {
+        let formData = data.data.data
+        let images = []
+        for (let i = 0; i < formData.images.length; i++) {
+          images.push(formData.images[i].imageUrl)
+        }
+
+        this.formData = {
+          ...formData,
+          images
+        };
+        this.publish = true;
+        this.publishType = "edit";
+      });
+    },
+    handleDraft() {},
+    preview() {
+      // this.$refs.quill.getData()
+      // console.log()
+    },
+    toDelete({ id }) {
+      this.$confirm("是否确认删除该条游记?", "警告", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      })
+        .then(() => {
+          deleteTravel(id).then(({ data }) => {
+            this.publish = false;
+            this.$notify({
+              title: "成功",
+              message: "删除成功",
+              type: "success",
+              duration: 2000,
+            });
+            this.getList();
+          });
+        })
+        .catch(function () {});
+    },
+    handleRefreshChange() {
+      this.getList(this.page);
+    },
+    cityView(newsId) {
+      newsOpenList({ newsId }).then(({ data }) => {
+        let cityList = data.data.data;
+        let allCityList = [];
+        let initCityList = [];
+        for (let i = 0; i < cityList.length; i++) {
+          allCityList.push({
+            cityId: cityList[i].cityId,
+            cityName: cityList[i].cityName,
+          });
+          if (cityList[i].isOpening) {
+            initCityList.push(cityList[i].cityId);
+          }
+        }
+        this.initCityList = initCityList;
+        this.allCityList = allCityList;
+        this.cityViewDialogVisible = true;
+      });
+    },
+  },
+};
+</script>
+<style lang="scss" scope>
+.user {
+  height: 100%;
+
+  &__tree {
+    padding-top: 3px;
+    padding-right: 20px;
+  }
+
+  &__main {
+    .el-card__body {
+      padding-top: 0;
+    }
+  }
+}
+.tag-list {
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+  .tag-list-item {
+    margin-bottom: 10px;
+    height: 30px;
+    line-height: 30px;
+    padding: 0 15px;
+    border: 1px solid #e9e9e9;
+    border-radius: 4px;
+    margin-right: 20px;
+  }
+}
+
+.form-title {
+  height: 60px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-bottom: 20px;
+  .form-title-name {
+    height: 60px;
+    line-height: 60px;
+    font-size: 20px;
+  }
+}
+</style>
+
