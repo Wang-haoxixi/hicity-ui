41867191ba8cb76e7af36ff4cbc8dae2fa3bfe04
wh <1570058176@qq.com>
2021-05-18 10:44:49 +0800

    广告路径管理页面搭建

---
+++ src/views/contentManage/addRoute/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <basic-container>
+        <div>建设中...</div>
+    </basic-container>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
