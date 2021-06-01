c20ca00955de18d9b3c9253cde5be4c4377fdc44
雷春路 <cc32131914@163.com>
2021-06-01 18:09:59 +0800

    店铺管理搜索条件修改

--- src/views/consumption/shopManage/index.vue
+++ src/views/consumption/shopManage/index.vue
@@ -13,6 +13,13 @@
             @click="toCreate"
             >新建</el-button>
         </template>
+
+        <template v-slot:searchItems="scope">
+          <div class="search-item">
+            <div style="white-space: nowrap;">城市/地区：</div>
+            <hc-city-select v-model="scope.searchForm.cityId" :city-id="userInfo.manageCityId" single></hc-city-select>
+          </div>
+        </template>
         <template slot="menu" slot-scope="scope">
           <template>
             <el-button type="text" size="mini" @click="toView(scope.row)"
